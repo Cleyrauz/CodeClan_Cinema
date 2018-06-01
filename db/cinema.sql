e586c8fd7d8d97ff85484992a1344e88a8b8327b
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-06-01 11:17:54 +0100

    Created tables

--- db/cinema.sql
+++ db/cinema.sql
@@ -3,5 +3,19 @@ DROP TABLES films;
 DROP TABLES customer;
 
 CREATE TABLE customers(
-  
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT4
+);
+
+CREATE TABLE films(
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT4
+);
+
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  customer_id REFERENCES customers(id) ON DELETE CASCADE,
+  film_id REFERENCES films(id) ON DELETE CASCADE
 );
