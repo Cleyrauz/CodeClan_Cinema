f5e1a21953f1a8adf16368c7bd25f5a59e6f28cd
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-06-03 12:48:29 +0100

    Added a method that finds out what is the most popular time

--- db/cinema.sql
+++ db/cinema.sql
@@ -15,14 +15,15 @@ CREATE TABLE films(
   price INT4
 );
 
-CREATE TABLE tickets(
-  id SERIAL4 PRIMARY KEY,
-  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
-  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
-);
-
 CREATE TABLE screenings(
   id SERIAL4 PRIMARY KEY,
   time_film VARCHAR(255),
   film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+);
+
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  screen_id INT4 REFERENCES screenings(id) ON DELETE CASCADE
 );
--- db/console.rb
+++ db/console.rb
@@ -5,6 +5,7 @@ require_relative('../models/screen')
 
 require('pry-byebug')
 
+Screen.delete_all()
 Ticket.delete_all()
 Film.delete_all()
 Customer.delete_all()
@@ -23,6 +24,13 @@ customer2 = Customer.new({
 
 customer2.save()
 
+customer3 = Customer.new({
+  'name' => 'Rosa',
+  'funds' => 10
+  })
+
+customer3.save()
+
 film1 = Film.new({
   'title' => 'Pulp Ficton',
   'price' => 10
@@ -37,32 +45,63 @@ film2 = Film.new({
 
 film2.save()
 
-ticket1 = Ticket.new({
-  'customer_id' => customer1.id,
-  'film_id' => film1.id
-  })
 
-  ticket1.save()
+  # customer1.name = "Janet"
+  # customer1.update()
+  #
+  # film2.title = "La la land"
+  # film2.update()
 
-ticket2 = Ticket.new({
-  'customer_id' => customer2.id,
-  'film_id' => film2.id
-  })
-
-  ticket2.save()
-
-
-  customer1.name = "Janet"
-  customer1.update()
-
-  film2.title = "La la land"
-  film2.update()
-
-  screen = Screen.new({
+  screen1 = Screen.new({
     'film_id' => film1.id,
     'time_film' => "December 31, 20:15"
     })
-  screen.save()
+  screen1.save()
+
+  screen2 = Screen.new({
+    'film_id' => film1.id,
+    'time_film' => "December 31, 18:10"
+    })
+    screen2.save()
+
+  screen3 = Screen.new({
+  'film_id' => film1.id,
+  'time_film' => "December 31, 14:30"
+  })
+  screen3.save()
+
+  # screen4 = Screen.new({
+  #   'film_id' => film2.id,
+  #   'time_film' => "December 31, 11:15"
+  #   })
+  #   screen4.save()
+
+    ticket1 = Ticket.new({
+      'customer_id' => customer1.id,
+      'film_id' => film1.id,
+      'screen_id' => screen1.id
+      })
+
+   ticket1.save()
+
+    ticket2 = Ticket.new({
+      'customer_id' => customer2.id,
+      'film_id' => film1.id,
+      'screen_id' => screen2.id
+      })
+
+    ticket2.save()
+
+    ticket3 = Ticket.new({
+    'customer_id' => customer3.id,
+    'film_id' => film1.id,
+    'screen_id' => screen1.id
+    })
+
+    ticket3.save()
+
+
+    ticket3.most_popular_time_by_film
 
   customer1.find_films_by_customer()
   film2.find_customers_by_films()
